4c53fde7e15d87fef5a13e312f08e455e0458cf0
andreabassi78 <andreabassi@polimi.it>
2020-04-26 10:31:19 +0200

    figures update

--- fit_gaussian2D.py
+++ fit_gaussian2D.py
@@ -22,10 +22,24 @@ x_lin = y_lin = np.arange(-1, 1.1, 0.1)
 X_np, Y_np = np.meshgrid(x_lin,y_lin)
 X0 = 0
 Y0 = 0
-w = 0.3 # waist
 
-Z_np = 0.5 * np.exp((- (X_np-X0)**2 - (Y_np-Y0)**2)/w**2)
-Z_np += 0.3 + 0.1* np.random.poisson(lam=1.0, size=Z_np.shape)
+A = 0.6 # amplitude
+W = 0.3 # waist
+B = 0.1 # bias
+N = 0.3 # noise amplitude
+
+def print_values(values, title=''):
+    print('\n', title, 'values:'
+              '\n --> Amplitude =', values[0],
+              '\n --> Waist =', values[1],
+              '\n --> Bias =', values[2], 
+              '\n'
+              )
+
+print_values([A,W,B], 'True values')
+
+Z_np = A * np.exp((- (X_np-X0)**2 - (Y_np-Y0)**2)/W**2)
+Z_np += B + N * (np.random.random(size = Z_np.shape)-0.5)
 
 x = torch.from_numpy(X_np).float().to(device = device)
 y = torch.from_numpy(Y_np).float().to(device = device)
@@ -33,35 +47,30 @@ z = torch.from_numpy(Z_np).float().to(device = device)
 
 # Define a net. 
 # Here I don't really exploit the layers of Pytorch,
-# I just define some parameters self.w 
+# I just define some parameters self.val 
 # that are optimized using the gaussian forward 
 class Net(torch.nn.Module):
 
-    def __init__(self, w_in):
+    def __init__(self, val_in):
         super(Net,self).__init__()
-        self.w = torch.nn.Parameter(w_in.clone()).to(device = device)
-        self.w.requires_grad = True
-        self.relu = torch.nn.ReLU()
+        self.val = torch.nn.Parameter(val_in.clone()).to(device = device)
+        self.val.requires_grad = True
+        #self.relu = torch.nn.ReLU()
         
     def forward(self, X, Y):
-        amp = self.w[0]
-        waist = self.w[1]
-        bias = self.w[2]
+        amp = self.val[0]
+        waist = self.val[1]
+        bias = self.val[2]
         X0 = 0
         Y0 = 0
         z_pred = ( bias + amp * torch.exp((- (X-X0)**2 - (Y-Y0)**2)/waist**2)
                   ).to(device = device)
-        return self.relu(z_pred) 
+        return z_pred 
     
-    def show_net_values(self, title = ''):
-        print('\n', title, 'values:'
-              '\n --> Amplitude =', self.w[0].detach().cpu().numpy(),
-              '\n --> Waist =', self.w[1].detach().cpu().numpy(),
-              '\n --> Bias =', self.w[2].detach().cpu().numpy(), 
-              '\n'
-              )
-
-
+    def print_net_values(self, title = ''):
+        print_values(self.val.detach().cpu().numpy(), title)
+        
+# use reasonable initialization values 
 amplitude_guess = np.amax(Z_np)
 waist_guess = np.std(Z_np)
 bkg_guess =  np.mean(Z_np)
@@ -71,11 +80,12 @@ initial_guess = torch.tensor([amplitude_guess,
                               bkg_guess
                               ]).to(device = device)
 
+# use random initialization values
 #initial_guess = torch.rand([3]).to(device = device)
 
 net = Net(initial_guess)
 
-net.show_net_values('Initial guess')
+net.print_net_values('Initilization')
 
 loss_fn = torch.nn.MSELoss(reduction='sum')
 
@@ -106,34 +116,41 @@ for t in range(50):
     # parameters
     optimizer.step()
 
-net.show_net_values('Fitted')    
+#print and plot the data
+net.print_net_values('Fitted')    
 
-def show_image(data, title=''):
-    plt.figure(figsize=(6, 6))
-    plt.gray()
-    plt.imshow(data,extent=[-1,1, -1,1])
-    plt.title(title)
-    plt.xlabel('x')
-    plt.ylabel('y')
 
+def show_image(ax, data, title='', vmin=0, vmax=1):
+    ax.imshow(data, interpolation='none',
+              cmap='gray',
+              origin='lower',
+              extent=[-1, 1, -1, 1],
+              vmax=vmax, vmin=vmin,
+              )
+    #ax.axis('equal')
+    ax.set(xlabel = 'x',
+           ylabel = 'y',
+           title = title,
+           )
+
+    
 input_data = z.detach().cpu().numpy()
 output_data = z_pred.detach().cpu().numpy()
 
-show_image(input_data,'Original')
-show_image(output_data,'Predicted')
+
+_fig, axs = plt.subplots(1,2,figsize=(12, 6))
+show_image(axs[0],input_data,'Original', vmin=0, vmax=input_data.max())
+show_image(axs[1],output_data,'Predicted', vmin=0, vmax=input_data.max())
 
 
-#plt.figure(figsize=(6, 6))
-#
-_fig, ax = plt.subplots()
+_fig, ax = plt.subplots(figsize=(6, 6))
 center_idx = int(input_data.shape[0]/2)
 ax.plot(x_lin, input_data[center_idx,:], 'gx', label = ('Original'))
 ax.plot(x_lin, output_data[center_idx,:], 'r-', label = ('Predicted'))
 ax.legend(loc='upper left', frameon=False)
 ax.grid()
-#ax.axis('equal')
 ax.set(xlabel = 'x',
        ylabel = 'z',
        xlim = (-1, 1),
-       title = 'Predicted vs Original'
+       title = 'Predicted vs Original at y = 0'
        )
